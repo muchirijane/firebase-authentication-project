5b6cce325280766f9a023a7648a0b70d0a872a33
jane muthoni <muchirijane@outlook.com>
2020-06-05 12:12:13 +0300

    Getting log in user info

--- scripts/auth.js
+++ scripts/auth.js
@@ -14,3 +14,31 @@ signUpForm.addEventListener('submit', (e) =>{
         signUpForm.reset();
     });
 });
+
+//logout
+const logout = document.querySelector('#logout');
+
+logout.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    auth.signOut().then(() =>{
+        console.log('User has logout!');
+    });
+});
+
+//login
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    //get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred =>{
+        console.log(cred);
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        signUpForm.reset();
+    })
+})
