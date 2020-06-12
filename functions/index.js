6fb9c2870642bdbbc8d116a5f72b1a0bb2e8c950
jane muthoni <muchirijane@outlook.com>
2020-06-12 23:41:31 +0300

    securing the cloud functions

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,11 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.addAdminRole = functions.https.onCall((data, context) =>{
+    if(context.auth.token.admin !== true){
+        return {
+            error: 'Only admins can add other admins, booyaah!'
+        }
+    }
     return admin.auth().getUserByEmail(data.email).then(user=>{
         return admin.auth().setCustomUserClaims(user.uid, {
             admin: true
